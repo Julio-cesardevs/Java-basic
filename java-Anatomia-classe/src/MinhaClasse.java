b2ca6d217074456cda955886e76dea96afc3ae24
cesar <103080100+chakaartt@users.noreply.github.com>
2024-04-23 23:09:12 -0300

    Anatomia_classe

---
+++ java-Anatomia-classe/src/MinhaClasse.java
@@ -0,0 +1,20 @@
+public class MinhaClasse {
+  // Aqui é corpo do meu Método
+  public static void main(String[] args) {
+    // Aqui vem minha variaveis , operaçoes e expressois etc...
+
+      // declação de variáveis.
+    String primeiroNome = "Júlio";
+    String segundoNome = "César";
+
+    String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+    System.out.println(nomeCompleto);
+      
+  }
+
+  // declarando um mètodo.
+  public static String nomeCompleto (String primeiroNome, String segundoNome) {
+    return " Resulatdo do método " + primeiroNome.concat(" ").concat(segundoNome);
+  }
+}
